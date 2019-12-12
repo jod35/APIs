250241d513a9575449464430eddd7f3d6d4f443a
jod35 <jodestrvin@gmail.com>
2019-12-12 09:40:25 +0300

    created the end points

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+app.debug=True
+app.secret_key='verysecret'
+
+
+#POST /store :data
+@app.route('/store')
+def create_store():
+    pass
+
+#GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+#GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+#POST /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def create_item_in_store(name):
+    pass
+
+#GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass 
+app.run(port=3000)
